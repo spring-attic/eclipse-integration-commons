2d26b5264a561eaa77611eaefab1a28416c4d6c7
Leo Dos Santos <leo.dos.santos@tasktop.com>
2012-08-01 17:23:45 -0700

    Startup extension reader should have early startup

--- org.springsource.ide.eclipse.commons.ui/src/org/springsource/ide/eclipse/commons/internal/ui/UiPlugin.java
+++ org.springsource.ide.eclipse.commons.ui/src/org/springsource/ide/eclipse/commons/internal/ui/UiPlugin.java
@@ -10,22 +10,11 @@
  *******************************************************************************/
 package org.springsource.ide.eclipse.commons.internal.ui;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExtension;
-import org.eclipse.core.runtime.IExtensionPoint;
-import org.eclipse.core.runtime.IExtensionRegistry;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.internal.WorkbenchPlugin;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-import org.springsource.ide.eclipse.commons.core.StatusHandler;
-import org.springsource.ide.eclipse.commons.ui.IIdeUiStartup;
 
 /**
  * @author Steffen Pingel
@@ -45,7 +34,6 @@ public class UiPlugin extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		StartupExtensionPointReader.runStartupExtensions();
 	}
 
 	@Override
@@ -68,49 +56,6 @@ public class UiPlugin extends AbstractUIPlugin {
 
 	public static IWorkbenchPage getActiveWorkbenchPage() {
 		return getActiveWorkbenchWindow().getActivePage();
-	}
-
-	static class StartupExtensionPointReader {
-
-		private static final String EXTENSION_ID_STARTUP = "com.springsource.sts.ide.ui.startup";
-
-		private static final String ELEMENT_STARTUP = "startup";
-
-		private static final String ELEMENT_CLASS = "class";
-
-		public static void runStartupExtensions() {
-			IExtensionRegistry registry = Platform.getExtensionRegistry();
-			IExtensionPoint extensionPoint = registry.getExtensionPoint(EXTENSION_ID_STARTUP);
-			IExtension[] extensions = extensionPoint.getExtensions();
-			for (IExtension extension : extensions) {
-				IConfigurationElement[] elements = extension.getConfigurationElements();
-				for (IConfigurationElement element : elements) {
-					if (element.getName().compareTo(ELEMENT_STARTUP) == 0) {
-						runStartupExtension(element);
-					}
-				}
-			}
-		}
-
-		private static void runStartupExtension(IConfigurationElement configurationElement) {
-			try {
-				Object object = WorkbenchPlugin.createExtension(configurationElement, ELEMENT_CLASS);
-				if (!(object instanceof IIdeUiStartup)) {
-					StatusHandler.log(new Status(IStatus.ERROR, UiPlugin.PLUGIN_ID, "Could not load "
-							+ object.getClass().getCanonicalName() + " must implement "
-							+ IIdeUiStartup.class.getCanonicalName()));
-					return;
-				}
-
-				IIdeUiStartup startup = (IIdeUiStartup) object;
-				startup.lazyStartup();
-			}
-			catch (CoreException e) {
-				StatusHandler.log(new Status(IStatus.ERROR, UiPlugin.PLUGIN_ID, "Could not load startup extension", e));
-			}
-
-		}
-
 	}
 
 }
