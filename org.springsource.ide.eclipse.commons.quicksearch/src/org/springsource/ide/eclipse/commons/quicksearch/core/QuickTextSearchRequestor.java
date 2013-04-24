f94a76e52c1886f7fc36480aa011e7db6733ec52
Kris De Volder <kdvolder@vmware.com>
2013-04-23 17:02:26 -0700

    Comment spelling

--- org.springsource.ide.eclipse.commons.quicksearch/src/org/springsource/ide/eclipse/commons/quicksearch/core/QuickTextSearchRequestor.java
+++ org.springsource.ide.eclipse.commons.quicksearch/src/org/springsource/ide/eclipse/commons/quicksearch/core/QuickTextSearchRequestor.java
@@ -16,9 +16,10 @@ package org.springsource.ide.eclipse.commons.quicksearch.core;
  * is some entity accepting the results of a search. Typically the requestor displays the
  * result to the user.
  * <p>
- * This API differs a little from the Eclipse SearchRequestor in that searches are 'live'
- * in the sense that they update while the user is typing. As the user is typing the
- * query is changing. This may result in results already results being changed or revoked.
+ * This API differs a little from the Eclipse SearchRequestor in that searches are 'live'.
+ * I.e the results are updating while the user is typing the query.
+ * As the query is changing, this may cause results that were added earlier being changed or 
+ * revoked.
  * 
  * @author Kris De Volder
  */
@@ -31,18 +32,21 @@ public class QuickTextSearchRequestor {
 
 	/**
 	 * Called when a previously added line of text needs to be redisplayed (this happens if
-	 * the query has changed but still matches the line. The line is still a match, but
+	 * the query has changed but still matches the line. I.e. the line is still a match, but
 	 * the highlighting of the search term is different.
 	 */
 	public void update(LineItem match) {}
 
 	/**
-	 * Called when a line of text previously added is no longer a match for the current query
+	 * Called when a line of text previously added is no longer a match for the current query.
+	 * I.e. the line should no longer be displayed.
 	 */
 	public void revoke(LineItem line) {}
 	
 	/**
-	 * Called when all previous results have become invalidated at once.
+	 * Called when all previous results have become revoked at once.
+	 * This happens when a query is changed in such a way that it can't be updated
+	 * incrementally but needs to be completely restarted.
 	 */
 	public void clear() {}
 }
