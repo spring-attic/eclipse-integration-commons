dec22e71f5c01317e33b7c715a0b47e2bff0fc07
Kris De Volder <kdevolder@gopivotal.com>
2013-09-03 16:24:05 -0700

    Get rid of spurious validation erros being displayed for a split second in a WizardPageWithSections.

--- org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/ui/WizardPageWithSections.java
+++ org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/ui/WizardPageWithSections.java
@@ -12,7 +12,9 @@ package org.springsource.ide.eclipse.commons.livexp.ui;
 
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -22,6 +24,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.progress.UIJob;
 import org.springsource.ide.eclipse.commons.livexp.core.CompositeValidator;
 import org.springsource.ide.eclipse.commons.livexp.core.LiveExpression;
 import org.springsource.ide.eclipse.commons.livexp.core.ValidationResult;
@@ -32,12 +35,28 @@ import org.springsource.ide.eclipse.commons.livexp.core.ValueListener;
  */
 public abstract class WizardPageWithSections extends WizardPage implements IPageWithSections, ValueListener<ValidationResult> {
 
+	/**
+	 * A delay used for posting status messages to the dialog area after a status update happens.
+	 * This is to get rid of spurious message that only appear for a fraction of a second as
+	 * internal some auto updating states in models are inconsistent. E.g. in new boot project wizard 
+	 * when project name is entered it is temporarily inconsistent with default project location until
+	 * that project location itself is update in response to the change event from the project name.
+	 * If the project location validator runs before the location update, a spurious validation error
+	 * temporarily results.
+	 * 
+	 * Note: this is a hacky solution. It would be better if the LiveExp framework solved this by
+	 * tracking and scheduling refreshes based on the depedency graph. Thus it might guarantee
+	 * that the validator never sees the inconsistent state because it is refreshed last.
+	 */
+	private static final long MESSAGE_DELAY = 250;
+
 	protected WizardPageWithSections(String pageName, String title, ImageDescriptor titleImage) {
 		super(pageName, title, titleImage);
 	}
 
 	private List<WizardPageSection> sections = null;
 	private CompositeValidator validator;
+	private UIJob updateJob;
 	
 	/* (non-Javadoc)
 	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
@@ -76,12 +95,18 @@ public abstract class WizardPageWithSections extends WizardPage implements IPage
 	protected abstract List<WizardPageSection> createSections();
 	
 	public void gotValue(LiveExpression<ValidationResult> exp, final ValidationResult status) {
+		setPageComplete(status.isOk()); //Don't delay this, never allow clicking finish button if state not consistent.
+		scheduleUpdateJob();
+	}
+	
+	private synchronized void scheduleUpdateJob() {
 		Shell shell = getShell();
 		if (shell!=null) {
-			Display display = shell.getDisplay();
-			if (display!=null) {
-				display.asyncExec(new Runnable() {
-					public void run() {
+			if (this.updateJob==null) {
+				this.updateJob = new UIJob("Update Wizard message") {
+					@Override
+					public IStatus runInUIThread(IProgressMonitor monitor) {
+						ValidationResult status = validator.getValue();
 						setErrorMessage(null);
 						setMessage(null);
 						if (status.isOk()) {
@@ -95,12 +120,15 @@ public abstract class WizardPageWithSections extends WizardPage implements IPage
 							setMessage(status.msg, IMessageProvider.NONE);
 						}
 						setPageComplete(status.isOk());
+						return Status.OK_STATUS;
 					}
-				});
+				};
+				updateJob.setSystem(true);
 			}
+			updateJob.schedule(MESSAGE_DELAY);
 		}
 	}
-	
+
 	public void dispose() {
 		for (WizardPageSection s : sections) {
 			s.dispose();
