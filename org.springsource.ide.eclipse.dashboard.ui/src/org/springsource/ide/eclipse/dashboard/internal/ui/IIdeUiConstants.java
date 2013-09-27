bf4a182b908f033eb9cf68cb602c7bca2e0d4f33
Leo Dos Santos <leo.dos.santos@tasktop.com>
2013-09-27 11:08:24 -0700

    Dashboard feed migration & re-enable preference UI

--- org.springsource.ide.eclipse.dashboard.ui/src/org/springsource/ide/eclipse/dashboard/internal/ui/IIdeUiConstants.java
+++ org.springsource.ide.eclipse.dashboard.ui/src/org/springsource/ide/eclipse/dashboard/internal/ui/IIdeUiConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 GoPivotal, Inc.
+ * Copyright (c) 2012 - 2013 GoPivotal, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -82,6 +82,6 @@ public interface IIdeUiConstants {
 
 	public static final int SECTION_EXPANDED = 2;
 
-
+	public static final String PREF_IO_BLOGFEED_MIGRATION = "dashboard.feed.springio.migration";
 
 }
