7771fe2dbf72acdeb96cc2b676374d5a2075b950
Kris De Volder <kdevolder@pivotal.io>
2015-01-23 13:12:04 -0800

    Function to compute width hint based on expected String length for text intput

--- org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/ui/UIConstants.java
+++ org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/ui/UIConstants.java
@@ -21,34 +21,38 @@ import org.eclipse.swt.widgets.Control;
 public class UIConstants {
 
 	/**
-	 * Used as hint for the width of a field label. This makes the UI look nicer 
+	 * Used as hint for the width of a field label. This makes the UI look nicer
 	 * as input boxes for fields tend to line up nicely.
 	 * <p>
-	 * Deprecated: use fieldLabelWidthHint() method instead to obtain a width hint that 
+	 * Deprecated: use fieldLabelWidthHint() method instead to obtain a width hint that
 	 * adapts to dialog font dimensions.
 	 */
 	@Deprecated
 	public static final int FIELD_LABEL_WIDTH_HINT = 80;
-	
+
 	public static final int FIELD_TEXT_AREA_WIDTH = 250;
-	
+
 	public static final int DIALOG_WIDTH_HINT = 310;
 
 	public static final int SCROLLABLE_LIST_HEIGTH = 200;
-	
+
 	/**
 	 * Computes a 'FIELD_LABEL_WIDTH_HINT' that scales based on the font metrics of
 	 * the 'DialogFont'. (See bug STS-3899)
 	 */
-	public static final int fieldLabelWidthHint(Control control) {
+	public static final int fieldLabelWidthHint(Control control, int numChars) {
 		Font dialogFont = JFaceResources.getDialogFont();
 		GC gc = new GC(control);
 		try {
 			gc.setFont(dialogFont);
-			return gc.getFontMetrics().getAverageCharWidth()*15;
+			return gc.getFontMetrics().getAverageCharWidth()*numChars;
 		} finally {
 			gc.dispose();
 		}
+	}
+
+	public static int fieldLabelWidthHint(Control control) {
+		return fieldLabelWidthHint(control, 15);
 	}
 
 }
