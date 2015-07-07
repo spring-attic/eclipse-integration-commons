b22e9fc357afcb3209be3f1500635cd6dd73f363
Kris De Volder <kdevolder@pivotal.io>
2015-07-06 17:49:36 -0700

    UIValueListener should use asyncExec instead of syncExec.

--- org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/core/UIValueListener.java
+++ org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/core/UIValueListener.java
@@ -13,20 +13,20 @@ package org.springsource.ide.eclipse.commons.livexp.core;
 import org.eclipse.swt.widgets.Display;
 
 /**
- * This class should be used instead of ValueListener when the code it 
- * wants to execute upon receiving a 'gotValue' event is required 
+ * This class should be used instead of ValueListener when the code it
+ * wants to execute upon receiving a 'gotValue' event is required
  * to run in the UIThread (i.e. typically this is code that needs to
  * update or read widgets in the UI).
  */
 public abstract class UIValueListener<T> implements ValueListener<T> {
-	
+
 	/**
 	 * This method is final. Implement 'uiGotValue' instead.
 	 */
 	public final void gotValue(final LiveExpression<T> exp, final T value) {
-		getDisplay().syncExec(new Runnable() {
+		getDisplay().asyncExec(new Runnable() {
 			public void run() {
-				uiGotValue(exp, value);				
+				uiGotValue(exp, value);
 			}
 		});
 	}
