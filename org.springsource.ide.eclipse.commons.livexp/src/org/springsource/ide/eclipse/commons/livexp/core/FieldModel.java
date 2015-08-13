a91c355c7822d87671e8373fe6f1be7b8ba26406
Kris De Volder <kdevolder@pivotal.io>
2015-08-13 10:39:14 -0700

    Some enhancements to LiveExp framework

--- org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/core/FieldModel.java
+++ org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/core/FieldModel.java
@@ -11,25 +11,27 @@
 package org.springsource.ide.eclipse.commons.livexp.core;
 
 public abstract class FieldModel<T> {
-	
+
 	private Class<T> type; //Type of data stored in the field.
 	private String name; // used to submit value to some service that handles the form
 	private String label; // Label to display in forms
 	private LiveVariable<T> variable;
 	private LiveExpression<ValidationResult> validator;
-	
+	private T defaultValue;
+
 	public FieldModel(Class<T> type, String name, T defaultValue) {
 		this.type = type;
 		this.name  = name;
 		this.label = name;
 		this.variable = new LiveVariable<T>(defaultValue);
 		this.validator = Validator.OK;
+		this.defaultValue = defaultValue;
 	}
-	
+
 	public Class<T> getType() {
 		return type;
 	}
-	
+
 	public String getLabel() {
 		return label;
 	}
@@ -41,7 +43,7 @@ public abstract class FieldModel<T> {
 	public LiveExpression<ValidationResult> getValidator() {
 		return validator;
 	}
-	
+
 	/**
 	 * Specify label (used in forms). By default the name is used.
 	 * @return The receiver for easy chaining.
@@ -59,13 +61,17 @@ public abstract class FieldModel<T> {
 	public void setValue(T v) {
 		this.variable.setValue(v);
 	}
-	
+
 	public T getValue() {
 		return variable.getValue();
 	}
-	
+
 	public LiveVariable<T> getVariable() {
 		return variable;
+	}
+
+	public T getDefaultValue() {
+		return defaultValue;
 	}
 
 }
--- org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/core/validators/UrlValidator.java
+++ org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/core/validators/UrlValidator.java
@@ -12,34 +12,45 @@ package org.springsource.ide.eclipse.commons.livexp.core.validators;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import org.springsource.ide.eclipse.commons.livexp.core.LiveExpression;
 import org.springsource.ide.eclipse.commons.livexp.core.LiveVariable;
+import org.springsource.ide.eclipse.commons.livexp.core.StringFieldModel;
 import org.springsource.ide.eclipse.commons.livexp.core.ValidationResult;
 
 /**
  * Basic validator for URLs. It merely verifies that the String can be parsed into a URI instance.
  * Some tolerance for leading and trailing whitespace is built-in (i.e. leading / trailing white space
  * is stripped off before validating).
- * 
+ *
  * A null value is treated the same as the empty String.
- * 
+ *
  * @author Kris De Volder
  */
 public class UrlValidator extends LiveExpression<ValidationResult> {
 
 	private String fieldName;
-	private LiveVariable<String> url;
+	private LiveExpression<String> url;
 	private boolean nullable;
+	private Set<String> allowedSchemes = null;
 
 	public UrlValidator(String fieldName, LiveVariable<String> url) {
 		this(fieldName, url, false);
 	}
-	
-	public UrlValidator(String fieldName, LiveVariable<String> url, boolean nullable) {
+
+	public UrlValidator(String fieldName, LiveExpression<String> url, boolean nullable) {
 		this.fieldName = fieldName;
 		this.url = url;
 		this.nullable = nullable;
+		dependsOn(url);
+	}
+
+	public UrlValidator(StringFieldModel field) {
+		this(field.getLabel(), field.getVariable());
 	}
 
 	@Override
@@ -52,14 +63,26 @@ public class UrlValidator extends LiveExpression<ValidationResult> {
 		}
 		if (nullable && str.equals("") ) {
 			return ValidationResult.OK;
+		} else if (!nullable && "".equals(str)) {
+			return ValidationResult.error(fieldName+" needs to be provided");
 		} else {
 			try {
-				new URI(str);
+				URI uri = new URI(str);
+				if (allowedSchemes!=null) {
+					if (!allowedSchemes.contains(uri.getScheme())) {
+						return ValidationResult.error(fieldName+" url scheme should be one of: "+allowedSchemes);
+					}
+				}
 			} catch (URISyntaxException e) {
 				return ValidationResult.error(fieldName+" is not a valid URL: URISyntaxException "+e.getMessage());
 			}
 		}
 		return ValidationResult.OK;
+	}
+
+	public UrlValidator allowedSchemes(String... schemes) {
+		this.allowedSchemes = Collections.unmodifiableSet(new LinkedHashSet<String>(Arrays.asList(schemes)));
+		return this;
 	}
 
 }
