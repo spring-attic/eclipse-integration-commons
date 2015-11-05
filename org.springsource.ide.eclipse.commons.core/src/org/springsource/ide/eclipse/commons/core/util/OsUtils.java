3ec479010fcf741e5645df0558b32da707b6ff72
Kris De Volder <kdevolder@pivotal.io>
2015-11-05 12:25:15 -0800

    Utility to convert from int to EnumSet of PoxixFilePermission

--- org.springsource.ide.eclipse.commons.core/src/org/springsource/ide/eclipse/commons/core/util/OsUtils.java
+++ org.springsource.ide.eclipse.commons.core/src/org/springsource/ide/eclipse/commons/core/util/OsUtils.java
@@ -10,9 +10,23 @@
  *******************************************************************************/
 package org.springsource.ide.eclipse.commons.core.util;
 
+import static java.nio.file.attribute.PosixFilePermission.GROUP_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.GROUP_READ;
+import static java.nio.file.attribute.PosixFilePermission.GROUP_WRITE;
+import static java.nio.file.attribute.PosixFilePermission.OTHERS_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.OTHERS_READ;
+import static java.nio.file.attribute.PosixFilePermission.OTHERS_WRITE;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_READ;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_WRITE;
+
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.EnumSet;
+import java.util.Set;
+
 /**
  * @author Kris De Volder
- * 
+ *
  * @since 2.8
  */
 public class OsUtils {
@@ -21,4 +35,29 @@ public class OsUtils {
 		return System.getProperty("os.name").contains("Windows");
 	}
 
+	private static final PosixFilePermission[] decodeMap = {
+			//This is exactly the reverse order as in the enum.
+			//But it seems safer not to rely on that.
+			OTHERS_EXECUTE,
+			OTHERS_WRITE,
+			OTHERS_READ,
+			GROUP_EXECUTE,
+			GROUP_WRITE,
+			GROUP_READ,
+			OWNER_EXECUTE,
+			OWNER_WRITE,
+			OWNER_READ
+	};
+
+	public static Set<PosixFilePermission> posixFilePermissions(int mode) {
+		int mask = 1;
+		Set<PosixFilePermission> perms = EnumSet.noneOf(PosixFilePermission.class);
+		for (PosixFilePermission flag : decodeMap) {
+			if (flag!=null && (mask & mode) != 0) {
+				perms.add(flag);
+			}
+			mask = mask << 1;
+		}
+		return perms;
+	}
 }
