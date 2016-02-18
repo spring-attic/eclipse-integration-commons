c63e7842317a9c31e4a3306423e28372d0ba5036
Kris De Volder <kdevolder@pivotal.io>
2016-02-18 14:56:07 -0800

    Generalize 'AsyncLiveExpression' so async behavior is optional.

--- org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/core/AsyncLiveExpression.java
+++ org.springsource.ide.eclipse.commons.livexp/src/org/springsource/ide/eclipse/commons/livexp/core/AsyncLiveExpression.java
@@ -16,8 +16,8 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 
 /**
- * Like a LiveExpression but ensures that its refresh method is always called
- * in a background job.
+ * Like a LiveExpression but has an option to ensures that its refresh
+ * method is always called in a background job.
  * <p>
  * With a regular LiveExp the refresh will be called on the same thread
  * as the change event it reacts to. So in general you have little control
@@ -34,29 +34,55 @@ import org.eclipse.core.runtime.jobs.Job;
  */
 public abstract class AsyncLiveExpression<T> extends LiveExpression<T> {
 
+	public enum AsyncMode {
+		SYNC, ASYNC
+	}
+
 	private Job refreshJob;
 	private long refreshDelay = 0;
 
 	public AsyncLiveExpression(T initialValue) {
-		this(initialValue, "AsyncLiveExpression refresh");
+		this(initialValue, AsyncMode.ASYNC);
 	}
 
+	public AsyncLiveExpression(T initialValue, AsyncMode mode) {
+		this(initialValue, mode==AsyncMode.ASYNC ? "AsyncLiveExpression refresh": null);
+	}
+
+	/**
+	 * Create AsyncLiveExpression. If Job name is passed in then
+	 * this expression will refresh itself asynchronously.
+	 * <p>
+	 * If the jobName is null then it behaves like a plain (i.e.
+	 * synchronous) LiveExpression.
+	 */
 	public AsyncLiveExpression(T initialValue, String jobName) {
 		super(initialValue);
-		refreshJob = new Job(jobName) {
-			protected IStatus run(IProgressMonitor monitor) {
-				syncRefresh();
-				return Status.OK_STATUS;
+		if (jobName!=null) {
+			refreshJob = new Job(jobName) {
+				protected IStatus run(IProgressMonitor monitor) {
+					syncRefresh();
+					return Status.OK_STATUS;
+				};
 			};
-		};
+		}
 	}
 
+	/**
+	 * This method is final, if you are overriding this, then you probably should
+	 * be overriding 'syncRefresh' instead. Otherwise you probably are breaking
+	 * async refresh support.
+	 */
 	@Override
-	public void refresh() {
-		refreshJob.schedule(refreshDelay);
+	public final void refresh() {
+		if (refreshJob!=null) {
+			refreshJob.schedule(refreshDelay);
+		} else {
+			syncRefresh();
+		}
 	}
 
-	private void syncRefresh() {
+	protected void syncRefresh() {
 		super.refresh();
 	}
 
