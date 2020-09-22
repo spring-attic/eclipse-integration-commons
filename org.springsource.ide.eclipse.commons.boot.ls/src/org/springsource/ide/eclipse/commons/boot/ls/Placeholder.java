ce0b751a23fccb1941defcc83702b096397a31e5
Kris De Volder <kdevolder@pivotal.io>
2020-09-22 11:25:21 -0700

    Create new empty commons.boot.ls plugin
    To be populated later with code related remote apps configuration.

---
+++ org.springsource.ide.eclipse.commons.boot.ls/src/org/springsource/ide/eclipse/commons/boot/ls/Placeholder.java
@@ -0,0 +1,8 @@
+package org.springsource.ide.eclipse.commons.boot.ls;
+
+/**
+ * So the package is not empty. Delete this class later when we have real code in here.
+ */
+public class Placeholder {
+
+}
